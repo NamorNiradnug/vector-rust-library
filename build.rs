90c5839385f7e4593e04e3c27c91edb726bd9942
NamorNiradnug <roma57linux@gmail.com>
2024-01-06 16:08:36 +0300

    use `cfg_aliases` in `build.rs`

--- build.rs
+++ build.rs
@@ -1,21 +1,12 @@
-use std::env;
-
-fn feature_shortcut(feature: &str, shortcut_name: &str) {
-    if env::var("CARGO_CFG_TARGET_FEATURE")
-        .map_or(false, |cfg| cfg.split(',').any(|f| f == feature))
-    {
-        println!("cargo:rustc-cfg={}", shortcut_name);
-    } else {
-        println!("cargo:rustc-cfg=no_{}", shortcut_name)
-    }
-}
-
-fn feature_shortcut_same(feature: &str) {
-    feature_shortcut(feature, feature);
-}
+use cfg_aliases::cfg_aliases;
 
 fn main() {
-    feature_shortcut_same("sse");
-    feature_shortcut("sse4.1", "sse41");
-    feature_shortcut_same("avx");
+    cfg_aliases! {
+        sse: { target_feature = "sse" },
+        no_sse: { not(sse) },
+        sse41: { target_feature = "sse4.1" },
+        no_sse41: { not(sse41) },
+        avx: { target_feature = "avx" },
+        no_avx: { not(avx) }
+    }
 }
